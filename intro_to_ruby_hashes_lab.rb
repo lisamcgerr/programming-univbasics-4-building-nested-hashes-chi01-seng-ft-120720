7c1b989951c5d6eb40f8cfddba9a04d0a442060b
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-23 15:55:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,15 @@ end
 
 
 def monopoly_with_third_tier
-
-
-
+monopoly_with_second_tier = {
+ base_hash = {
+    :railroads => {
+    :pieces => 4,
+    :rent_in_dollars => { },
+    :names => { }
+    } 
+}
+}
 end
  
  
